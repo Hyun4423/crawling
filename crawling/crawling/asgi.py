25bfe23bc99f4c9274b5d17c76920353bdfb9dc4
조우현 <cube684423@gmail.com>
2024-02-20 10:59:39 +0900

    1

--- crawling/crawling/asgi.py
+++ crawling/crawling/asgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from uvicorn import run
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crawling.settings')
 
 application = get_asgi_application()
+
+if __name__ == "__main__":
+    run(application, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
