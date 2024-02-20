a310ab0cf9f9d95de8c2893906c6ad9658a4f774
조우현 <cube684423@gmail.com>
2024-02-20 12:52:35 +0900

    test

--- crawling/search/views.py
+++ crawling/search/views.py
@@ -6,6 +6,7 @@ import requests
 import json
 from .models import Search
 import random
+import webbrowser
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
@@ -14,59 +15,61 @@ def index(request):
 
     goodsList = []
 
-    for obj in searchList:
+    webbrowser.open_new("https://search.shopping.naver.com/search/all")
 
-        url = "https://search.shopping.naver.com/search/all"
-        keyword = obj.keyword
-
-        # params = None
-        # if url.find('naver'):
-        params = (
-            ('sort', 'price_asc'),
-            ('pagingIndex', '1'),
-            ('pagingSize', '3'),
-            ('viewType', 'list'),
-            ('productSet', 'total'),
-            ('deliveryFee', ''),
-            ('deliveryTypeValue', ''),
-            ('frm', 'NVSHATC'),
-            ('query', keyword),
-            ('origQuery', keyword),
-            ('freeDelivery', 'true'),
-            ('iq', ''),
-            ('eq', ''),
-            ('xq', ''),
-        )
-
-        # proxy_server = random_us_proxy()
-        # proxies = {"http": 'http://' + proxy_server, 'https': 'https://' + proxy_server}
-        # res = requests.get(url, params=params, headers=headers, proxies=proxies)
-
-        res = requests.get(url, params=params, headers=headers, verify=False)
-        html = res.text
-
-        soup = BeautifulSoup(html, 'html.parser')
-
-        content = soup.select_one("#content")
-
-        items = content.select("div.basicList_list_basis__uNBZx > div > div")
-
-        for item in items:
-            title = item.select_one("div.product_title__Mmw2K > a").text
-            link = item.select_one("div.product_title__Mmw2K > a").get("href")
-            price = item.select_one("div.product_price_area__eTg7I span.price").text
-            dlv = item.select_one("div.product_price_area__eTg7I span.price_delivery__yw_We").text
-            comp = item.select_one("div.product_mall_title__Xer1m > a > img")
-            compImg = ""
-            compText = ""
-            if comp:
-                compImg = comp.get("src")
-                compText = comp.get("alt")
-            else:
-                compText = item.select_one("div.product_mall_title__Xer1m > a").text
-
-            goodsList.append(
-                {"title": title, "link": link, "price": price, "dlv": dlv, "compImg": compImg, "compText": compText})
+    # for obj in searchList:
+    #
+    #     url = "https://search.shopping.naver.com/search/all"
+    #     keyword = obj.keyword
+    #
+    #     # params = None
+    #     # if url.find('naver'):
+    #     params = (
+    #         ('sort', 'price_asc'),
+    #         ('pagingIndex', '1'),
+    #         ('pagingSize', '3'),
+    #         ('viewType', 'list'),
+    #         ('productSet', 'total'),
+    #         ('deliveryFee', ''),
+    #         ('deliveryTypeValue', ''),
+    #         ('frm', 'NVSHATC'),
+    #         ('query', keyword),
+    #         ('origQuery', keyword),
+    #         ('freeDelivery', 'true'),
+    #         ('iq', ''),
+    #         ('eq', ''),
+    #         ('xq', ''),
+    #     )
+    #
+    #     # proxy_server = random_us_proxy()
+    #     # proxies = {"http": 'http://' + proxy_server, 'https': 'https://' + proxy_server}
+    #     # res = requests.get(url, params=params, headers=headers, proxies=proxies)
+    #
+    #     res = requests.get(url, params=params, headers=headers, verify=False)
+    #     html = res.text
+    #
+    #     soup = BeautifulSoup(html, 'html.parser')
+    #
+    #     content = soup.select_one("#content")
+    #
+    #     items = content.select("div.basicList_list_basis__uNBZx > div > div")
+    #
+    #     for item in items:
+    #         title = item.select_one("div.product_title__Mmw2K > a").text
+    #         link = item.select_one("div.product_title__Mmw2K > a").get("href")
+    #         price = item.select_one("div.product_price_area__eTg7I span.price").text
+    #         dlv = item.select_one("div.product_price_area__eTg7I span.price_delivery__yw_We").text
+    #         comp = item.select_one("div.product_mall_title__Xer1m > a > img")
+    #         compImg = ""
+    #         compText = ""
+    #         if comp:
+    #             compImg = comp.get("src")
+    #             compText = comp.get("alt")
+    #         else:
+    #             compText = item.select_one("div.product_mall_title__Xer1m > a").text
+    #
+    #         goodsList.append(
+    #             {"title": title, "link": link, "price": price, "dlv": dlv, "compImg": compImg, "compText": compText})
 
     context = {"success": "success", "goodsList": goodsList, "searchList": searchList}
 
